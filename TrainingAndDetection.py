b2215dd0d2256e7c98e670f4651a999f1195146e
Janez Vegan <janez.vegan@gmail.com>
2022-05-29 22:30:14 +0200

    python program za namestitev tensorflow

---
+++ TrainingAndDetection.py
@@ -0,0 +1,56 @@
+import wget
+import os
+
+# ***********************************************************************************************************
+# 0. Setup Paths
+# ***********************************************************************************************************
+# Variables od Pretrained models
+CUSTOM_MODEL_NAME = 'my_ssd_mobnet'
+PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'
+# link do Pretrained models (iz tega linka ga lahko snameš)
+PRETRAINED_MODEL_URL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz'
+TF_RECORD_SCRIPT_NAME = 'generate_tfrecord.py'
+LABEL_MAP_NAME = 'label_map.pbtxt'
+
+# Shrani vse mape poti v dictionary paths
+paths = {
+    'WORKSPACE_PATH': os.path.join('Tensorflow', 'workspace'),
+    'SCRIPTS_PATH': os.path.join('Tensorflow', 'scripts'),
+    'APIMODEL_PATH': os.path.join('Tensorflow', 'models'),
+    'ANNOTATION_PATH': os.path.join('Tensorflow', 'workspace', 'annotations'),
+    'IMAGE_PATH': os.path.join('Tensorflow', 'workspace', 'images'),
+    'MODEL_PATH': os.path.join('Tensorflow', 'workspace', 'models'),
+    'PRETRAINED_MODEL_PATH': os.path.join('Tensorflow', 'workspace', 'pre-trained-models'),
+    'CHECKPOINT_PATH': os.path.join('Tensorflow', 'workspace', 'models', CUSTOM_MODEL_NAME),
+    'OUTPUT_PATH': os.path.join('Tensorflow', 'workspace', 'models', CUSTOM_MODEL_NAME, 'export'),
+    'TFJS_PATH': os.path.join('Tensorflow', 'workspace', 'models', CUSTOM_MODEL_NAME, 'tfjsexport'),
+    'TFLITE_PATH': os.path.join('Tensorflow', 'workspace', 'models', CUSTOM_MODEL_NAME, 'tfliteexport'),
+    'PROTOC_PATH': os.path.join('Tensorflow', 'protoc')
+}
+
+# Files dictionary
+files = {
+    'PIPELINE_CONFIG': os.path.join('Tensorflow', 'workspace', 'models', CUSTOM_MODEL_NAME, 'pipeline.config'),
+    'TF_RECORD_SCRIPT': os.path.join(paths['SCRIPTS_PATH'], TF_RECORD_SCRIPT_NAME),
+    'LABELMAP': os.path.join(paths['ANNOTATION_PATH'], LABEL_MAP_NAME)
+}
+
+# Kreiraj vse mape in podmape zapisane v Dictionary paths
+# mode
+for path in paths.values():
+    if not os.path.exists(path):
+        if os.name == 'posix':
+            # Linux-Python ukaz- ne vem, če dela
+            os.makedirs(path)
+        if os.name == 'nt':
+            os.makedirs(path)
+
+# ***********************************************************************************************************
+# 1. Download TF Models Pretrained Models from Tensorflow Model Zoo and Install TFOD
+# ********************************************************************************************************
+if not os.path.exists(os.path.join(paths['APIMODEL_PATH'], 'research', 'object_detection')):
+    # Iz dictionary sestavi string spremenljivko git command (natejena tmp2string, direkt vrednost iz dictionary vrže napako)
+    gitCommand = 'git clone https://github.com/tensorflow/models ' + \
+        paths['APIMODEL_PATH']
+    # izvedi git komando v CMD-ju
+    os.system(gitCommand)
